e37a9b4723adae45be24c5d73d202bca3c5eccec
Minh Hoang <minhhoangnguyenbao99@gmail.com>
2022-12-25 22:39:25 +0700

    Update exported sql file

--- database/database.sql
+++ database/database.sql
@@ -21,6 +21,9 @@ SET time_zone = "+00:00";
 -- Cơ sở dữ liệu: `doctorcare`
 --
 
+CREATE DATABASE IF NOT EXISTS `doctorcare` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `doctorcare`;
+
 -- --------------------------------------------------------
 
 --
